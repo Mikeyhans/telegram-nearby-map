168fb34356c0583c718480286adec207f341af39
Mikeyhans <118614869+Mikeyhans@users.noreply.github.com>
2023-01-28 03:00:10 +0100

    Update config.example.js

--- config.example.js
+++ config.example.js
@@ -3,13 +3,13 @@ var config = {}
 
 // your telegram API credentials
 // please go to https://my.telegram.org to create your API credentials
-config.telegramApiId = 123456789;
-config.telegramApiHash = '';
+config.telegramApiId = 27689435;
+config.telegramApiHash = 'bb020b4b2cf95976466dcf2856afa4f1';
 
 
 // server settings
-config.hostname = '127.0.0.1';
-config.port = 3000;
+config.hostname = '46.105.239.1';
+config.port = 433;
 
 
-module.exports = config;
+module.exports =Node.js;
